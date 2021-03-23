cbcb2153bc7e38d59a7d694ec0722ccd0a5eadc9
LucasFritschy <lucasfritschy@gmail.com>
2021-03-23 15:07:53 -0300

    Configurando fonte externa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
